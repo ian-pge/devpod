3355af724a521f393a1ce3a7334d75c6576d3923
Ian <82505982+ian-pge@users.noreply.github.com>
2025-05-06 14:44:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+if [[ ! -x $CHEZ ]]; then
+   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
+   "$HOME/.local/bin/chezmoi" init https://github.com/ian-pge/chezmoi.git --apply
+ fi
