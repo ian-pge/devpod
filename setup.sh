59d0762fff12d899660d4b05c884bfe954c8aeb6
Ian <82505982+ian-pge@users.noreply.github.com>
2025-05-06 14:46:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ CHEZ="$HOME/.local/bin/chezmoi"
 if [[ ! -x $CHEZ ]]; then
    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
    "$CHEZ" init https://github.com/ian-pge/chezmoi.git --apply
- fi
+fi
