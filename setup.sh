f62f224b28d284ddaf04ab22a435ded7b4cfff82
ian <ian.page38@gmail.com>
2025-12-17 00:59:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,3 @@ cd "${HOME}/dotfiles"
 
 out="$(nix build ".#homeConfigurations.zed.activationPackage" --no-link --print-out-paths)"
 "$out/activate"
-
-# 6. Change default shell to fish
-CURRENT_SHELL="$(getent passwd "$(whoami)" | cut -d: -f7)"
-if [[ "$CURRENT_SHELL" != "/bin/fish" ]]; then
-  log "Changing default shell to fish..."
-  sudo usermod -s /bin/fish "$(whoami)"
-fi
