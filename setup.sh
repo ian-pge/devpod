0a45ad35e9788e5d4b8f6a92e11e6a1820042a7e
ian <ian.page38@gmail.com>
2025-12-17 00:53:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# # Make sure nix is found even very early in container setup
-# export PATH="/home/zed/.nix-profile/bin:/nix-profile/bin:${PATH}"
-
-# # Make nix behave consistently
-# export NIX_REMOTE="${NIX_REMOTE:-daemon}"
-# export NIX_SSL_CERT_FILE="${NIX_SSL_CERT_FILE:-/etc/ssl/certs/ca-certificates.crt}"
-# export NIX_CONFIG="${NIX_CONFIG:-experimental-features = nix-command flakes
-# accept-flake-config = true}"
-
 cd "${HOME}/dotfiles"
 
 out="$(nix build ".#homeConfigurations.zed.activationPackage" --no-link --print-out-paths)"
 "$out/activate"
+
+# 6. Change default shell to fish
+CURRENT_SHELL="$(getent passwd "$(whoami)" | cut -d: -f7)"
+if [[ "$CURRENT_SHELL" != "/bin/fish" ]]; then
+  log "Changing default shell to fish..."
+  sudo usermod -s /bin/fish "$(whoami)"
+fi
