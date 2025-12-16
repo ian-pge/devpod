909d15c1e065a6dc11cb2db6e832aeebcb22148b
ian <ian.page38@gmail.com>
2025-12-17 00:50:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Make sure nix is found even very early in container setup
-export PATH="/home/zed/.nix-profile/bin:/nix-profile/bin:${PATH}"
+# # Make sure nix is found even very early in container setup
+# export PATH="/home/zed/.nix-profile/bin:/nix-profile/bin:${PATH}"
 
-# Make nix behave consistently
-export NIX_REMOTE="${NIX_REMOTE:-daemon}"
-export NIX_SSL_CERT_FILE="${NIX_SSL_CERT_FILE:-/etc/ssl/certs/ca-certificates.crt}"
-export NIX_CONFIG="${NIX_CONFIG:-experimental-features = nix-command flakes
-accept-flake-config = true}"
+# # Make nix behave consistently
+# export NIX_REMOTE="${NIX_REMOTE:-daemon}"
+# export NIX_SSL_CERT_FILE="${NIX_SSL_CERT_FILE:-/etc/ssl/certs/ca-certificates.crt}"
+# export NIX_CONFIG="${NIX_CONFIG:-experimental-features = nix-command flakes
+# accept-flake-config = true}"
 
 cd "${HOME}/dotfiles"
 
